39b3ec94abc5b0025bdbfed627d0ca5303cf416a
Cleo Gao <celeo127@outlook.com>
2023-05-20 19:37:29 +0800

    added route for recipe and create recipe

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from '../models/User.js';
 
 
 const router = express.Router();
@@ -19,6 +20,19 @@ router.post("/", async(req, res) => {
     try{
         const response = await recipe.save();
         res.json(response);
+    }catch (err){
+        res.json(err);
+    }
+});
+
+router.put("/", async(req, res) => {
+   
+    try{
+        const recipe = await RecipeModel.findById(req.body.recipeID);
+        const user = await UserModel.findById(req.body.userID);
+        user.savedRecipes.push(recipe);
+        await user.save();
+        res.json({savedRecipes: user.savedRecipes});
     }catch (err){
         res.json(err);
     }
